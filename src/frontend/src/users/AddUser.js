27f4958779784aa7486a0d27039e441dfe438147
SeongJin Oh <guk0230@naver.com>
2023-04-27 18:58:30 +0900

    AddUser form 생성

--- src/frontend/src/users/AddUser.js
+++ src/frontend/src/users/AddUser.js
@@ -1,7 +1,54 @@
-import React from " Formreact";
-
+import React from "react";
+/* className="" 따옴표안에 css 자동완성 => 마켓 플레이스에서 IntelliSense for CSS class names in HTML 확장 프로그램 다운 */
 function AddUser() {
-  return <div>AddUser Form</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Register User</h2>
+          <div className="mb-3">
+            <label htmlFor="Name" className="form-label">
+              Name
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your name"
+              name="name"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="Username" className="form-label">
+              Username
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your username"
+              name="username"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              E-mail
+            </label>
+            <input
+              type={"text"}
+              className="form-control"
+              placeholder="Enter your e-mail address"
+              name="email"
+            />
+          </div>
+          <button type="submit" className="btn btn-outline-primary">
+            submit
+          </button>
+          <button type="submit" className="btn btn-outline-danger mx-2">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AddUser;
